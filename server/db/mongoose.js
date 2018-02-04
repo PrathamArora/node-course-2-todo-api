4e47045bbc4edb5b1b0d8b7ae462ecbf185bcd42
Pratham Arora <arorapratham6@gmail.com>
2018-02-04 15:29:13 +0530

    Setup separate and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,3 +8,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 module.exports = {
   mongoose : mongoose
 };
+
+
+// process.env.NODE_ENV = 'production'
+// process.env.NODE_ENV = 'development'
+// process.env.NODE_ENV = 'test'
